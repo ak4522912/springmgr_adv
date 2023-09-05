97a03f2d15cf821b5b4cdd74f910976cd36d6a52
amitkumar16 <amitkumar@fynd-external.com>
2023-09-05 12:02:05 +0530

    Custom methods on repository layer
    Signed-off-by: amitkumar16 <amitkumar@fynd-external.com>

--- src/main/java/com/basic/springmgr_adv/respositories/TasksRepository.java
+++ src/main/java/com/basic/springmgr_adv/respositories/TasksRepository.java
@@ -2,8 +2,24 @@ package com.basic.springmgr_adv.respositories;
 
 import com.basic.springmgr_adv.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TasksRepository extends JpaRepository<TaskEntity,Integer> {
+    List<TaskEntity> findAllByCompleted(boolean completed);
+    //ideally this is business logic terminolog(i.e overdue) so should be in service
+    @Query(""+
+    "Select t from tasks t"+
+    "whete t.completed = false"+
+    "AND t.dueDate <= CURRENT_DATE")
+    List<TaskEntity> findAllOverdue();
+
+    @Query(""+
+    "Select t from tasks t where t.title LIKE %?1%")
+    List<TaskEntity> findAllByTitle(String title);
+
+    List<TaskEntity> findAllByTitleIsContainingIgnoreCase(String title);
 }
--- src/test/java/com/basic/springmgr_adv/repositories/TasksRepositoryTests.java
+++ src/test/java/com/basic/springmgr_adv/repositories/TasksRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
@@ -20,5 +21,40 @@ public class TasksRepositoryTests {
         task.setCompleted(false);
         var savedTask = tasksRepository.save(task);
         assertNotNull(savedTask);
+    }
+
+    @Test
+    public void addTwoTasks(){
+        TaskEntity task1 = new TaskEntity();
+        task1.setTitle("Test Task");
+        task1.setDescription("Test Descr");
+        task1.setCompleted(false);
+        var savedTask1 = tasksRepository.save(task1);
+        TaskEntity task2 = new TaskEntity();
+        task2.setTitle("Test Task");
+        task2.setDescription("Test Descr");
+        task2.setCompleted(false);
+        var savedTask2 = tasksRepository.save(task2);
+        var tasks = tasksRepository.findAll();
+        assertNotNull(tasks);
+        assertEquals(2,tasks.size());
+    }
+
+    @Test
+    public void findByCompletedWorks(){
+        TaskEntity task1 = new TaskEntity();
+        task1.setTitle("Test Task");
+        task1.setDescription("Test Descr");
+        task1.setCompleted(false);
+        var savedTask1 = tasksRepository.save(task1);
+        TaskEntity task2 = new TaskEntity();
+        task2.setTitle("Test Task");
+        task2.setDescription("Test Descr");
+        task2.setCompleted(true);
+        var savedTask2 = tasksRepository.save(task2);
+        var completedTasks = tasksRepository.findAllByCompleted(true);
+        var nonCompletedTasks = tasksRepository.findAllByCompleted(false);
+        assertEquals(1,completedTasks.size());
+        assertEquals(1,nonCompletedTasks.size());
     }
 }
